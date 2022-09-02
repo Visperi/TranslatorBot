ccd5ab3f79b618ae081d8d9c40a895c351ddeacc
Visperi <niksuposti@gmail.com>
2022-09-02 13:27:40 +0300

    Make command hybrid and guild only

--- cogs/commands.py
+++ cogs/commands.py
@@ -112,7 +112,8 @@ class TranslationCog(commands.Cog):
         except ValueError as e:
             await ctx.send(str(e))
 
-    @commands.command(name="languages", description="Get list of all supported language abbreviations.")
+    @commands.guild_only()
+    @commands.hybrid_command(name="languages", description="Get list of all supported language abbreviations.")
     async def get_supported_languages(self, ctx: commands.Context):
         formatted = [f"`{abbr}`" for abbr in self.supported_languages_abbr]
         await ctx.send(", ".join(formatted))
