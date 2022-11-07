00696906212a2a27a6fc1d47abf8f1043b188515
Visperi <niksuposti@gmail.com>
2022-11-07 16:55:31 +0200

    Add missing empty line

--- deepl/errors.py
+++ deepl/errors.py
@@ -22,6 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
+
 class DeepLError(Exception):
     """Base exception for DeepL API related exceptions"""
     pass
